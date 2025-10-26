5a25a56923caa475100a4c95a6ba8652035880f8
구혁모 <gurah129@gmail.com>
2025-10-26 19:46:25 +0900

    🎨 코드 포맷팅 전체 적용 및 초기 세팅 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,7 +11,19 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 공용 변수/믹스인 파일을 여기에 등록
+        // 모든 .scss 파일에서 @import 없이 $primary-color 등을 사용 가능
+        additionalData: `
+          @import "@/styles/_variables.scss";
+          @import "@/styles/_function.scss";
+        `,
+      },
     },
   },
 });
