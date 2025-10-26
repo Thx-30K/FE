ee81eaf0116da17f21cd03f9102f6ffe78e48b87
구혁모 <gurah129@gmail.com>
2025-10-26 19:49:36 +0900

    🍱 로고, 아이콘 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import logo from './logo.svg';
+import searchIcon from './search-icon.svg';
+import historyIcon from './history-icon.svg';
+import historyBackIcon from './history-back-icon.svg';
+
+export { logo, searchIcon, historyIcon, historyBackIcon };
