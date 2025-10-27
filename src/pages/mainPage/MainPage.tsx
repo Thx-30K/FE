c32684d501933bff2b204d454b32853a95db7579
a-neey <2371429@hansung.ac.kr>
2025-10-27 17:03:51 +0900

    🍱 배경 이미지 추가

---
+++ src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './styles/MainPage.module.scss';
+
+const MainPage = () => {
+  return <div className={s.mainContainer}></div>;
+};
+
+export default MainPage;
