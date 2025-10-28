3dd791082383f7851115d124882ffda3f08d5a01
a-neey <2371429@hansung.ac.kr>
2025-10-28 13:56:15 +0900

    💬 푸터 임시 내용 추가

--- src/pages/mainPage/components/Footer.tsx
+++ src/pages/mainPage/components/Footer.tsx
@@ -7,11 +7,13 @@ const Footer = () => {
   return (
     <div className={s.footerContainer}>
       <img src={LOGO} alt="logo" className={s.footerImg} />
+
+      {/* 임시 푸터 내용 */}
       <div className={s.footerText}>
-        <p>ⓒ HSU [데이터(삼)만개감사합니다] 팀 </p>
-        <br />
         <p>FE 구혁모 김예나</p>
         <p>BE 이가은 조주한 천성진</p>
+        <br />
+        <p>ⓒ HSU 25-2 기업연계캡스톤디자인 [데이터(삼)만개감사합니다] 팀 </p>
       </div>
     </div>
   );
