7f795ade15c5a3d873a55fc120e83f3950f16cf5
구혁모 <gurah129@gmail.com>
2025-10-29 23:35:57 +0900

    ✨ 대시보드 UI 구현(차트 제외)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
+import { DashBoardPage } from './pages/DashBoardPage/DashBoardPage';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           {/* <Route path="/" element={<MainPage />} /> */}
-          {/* <Route path="/dashboard" element={<DashBoardPage />} /> */}
+          <Route path="/dashboard" element={<DashBoardPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
