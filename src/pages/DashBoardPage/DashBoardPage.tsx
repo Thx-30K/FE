02abf9c5ec894cddbae7d073dbc43d9962d4516a
구혁모 <gurah129@gmail.com>
2025-11-23 17:13:36 +0900

    ♻️ 검색 기록 사이드바 레이아웃에서 사용

--- src/pages/DashBoardPage/DashBoardPage.tsx
+++ src/pages/DashBoardPage/DashBoardPage.tsx
@@ -9,7 +9,6 @@ import { useState } from 'react';
 import { CardDetail } from './components/CardDetail/CardDetail';
 import type { SearchData } from '@/types/Card';
 import { ExportSelect } from './components/ExportSelect/ExportSelect';
-import SideBar from '@/components/SideBar/SideBar';
 import { PanelTable } from './components/Table/PanelTable';
 
 export const DashBoardPage = () => {
@@ -20,87 +19,83 @@ export const DashBoardPage = () => {
   };
 
   return (
-    <>
-      {' '}
-      <SideBar />
-      <div className={styles.container}>
-        {/* 카드 클릭 시 */}
-        {cardDetailVisible && (
-          <CardDetail
-            data={cards[0]}
-            onClick={() => setCardDetailVisible(false)}
-          />
-        )}
-        {/* 검색 및 카드 영역 */}
-        <div className={styles.topContent}>
-          <img src={logo} className={styles.logo} alt="Logo" />
-          <SearchBar onSearchSubmit={handleSearch} />
-          <div className={styles.searchSummary}>
-            <div className={styles.SummaryTags}>
-              {tags.map((tag, index) => (
-                <span key={index} className={styles.SummaryTag}>
-                  #{tag}
-                </span>
-              ))}
-            </div>
-            <div className={styles.resultCount}>검색된 패널 : {100}명</div>
+    <div className={styles.container}>
+      {/* 카드 클릭 시 */}
+      {cardDetailVisible && (
+        <CardDetail
+          data={cards[0]}
+          onClick={() => setCardDetailVisible(false)}
+        />
+      )}
+      {/* 검색 및 카드 영역 */}
+      <div className={styles.topContent}>
+        <img src={logo} className={styles.logo} alt="Logo" />
+        <SearchBar onSearchSubmit={handleSearch} />
+        <div className={styles.searchSummary}>
+          <div className={styles.SummaryTags}>
+            {tags.map((tag, index) => (
+              <span key={index} className={styles.SummaryTag}>
+                #{tag}
+              </span>
+            ))}
           </div>
-          <div className={styles.recommandPanel}>
-            <div className={styles.recommandPanelTitle}>
-              이런 패널들은 어때요?
-            </div>
-            <div className={styles.cardList}>
-              {cards.map((card) => (
-                <Card
-                  key={card.id}
-                  data={card}
-                  onClick={() => setCardDetailVisible(true)}
-                />
-              ))}
-            </div>
-          </div>
+          <div className={styles.resultCount}>검색된 패널 : {100}명</div>
         </div>
-        {/* TODO: 그래프 영역 */}
-        <div className={styles.middleContent}>
-          <div className={styles.doughnutContainer}>
-            <div className={styles.doughnutTitle}>{'검색 결과'}</div>
-            <div className={styles.doughnutSection}>
-              {/* 차트 들어갈 곳 */}
-              <DoughnutChart />
-            </div>
+        <div className={styles.recommandPanel}>
+          <div className={styles.recommandPanelTitle}>
+            이런 패널들은 어때요?
           </div>
-          <div className={styles.barContainer}>
-            <div className={styles.barSection}>
-              <div className={styles.barTitle}>{'나이대별 평균 소득'}</div>
-              <div className={styles.barContent}>
-                {/* 차트 들어갈 곳 */}
-                <LineAreaChart />
-              </div>
-            </div>
-            <div className={styles.barSection}>
-              <div className={styles.barTitle}>{'바 차트 이름'}</div>
-              <div className={styles.barContent}>
-                {/* 차트 들어갈 곳 */}
-                <BarChart />
-              </div>
-            </div>
+          <div className={styles.cardList}>
+            {cards.map((card) => (
+              <Card
+                key={card.id}
+                data={card}
+                onClick={() => setCardDetailVisible(true)}
+              />
+            ))}
           </div>
-          <div className={styles.blankBox}></div>
-          <div className={styles.blankBox}></div>
-        </div>
-        {/* 표 영역 */}
-        <div className={styles.bottomContent}>
-          <div className={styles.bottomTitle}>
-            <span className={styles.resultCount}>검색된 패널 : {100}명</span>
-            <div className={styles.exportContainer}>
-              <ExportSelect />
-              <div className={styles.exportButton}>내보내기</div>
-            </div>
-          </div>
-          <PanelTable />
         </div>
       </div>
-    </>
+      {/* TODO: 그래프 영역 */}
+      <div className={styles.middleContent}>
+        <div className={styles.doughnutContainer}>
+          <div className={styles.doughnutTitle}>{'검색 결과'}</div>
+          <div className={styles.doughnutSection}>
+            {/* 차트 들어갈 곳 */}
+            <DoughnutChart />
+          </div>
+        </div>
+        <div className={styles.barContainer}>
+          <div className={styles.barSection}>
+            <div className={styles.barTitle}>{'나이대별 평균 소득'}</div>
+            <div className={styles.barContent}>
+              {/* 차트 들어갈 곳 */}
+              <LineAreaChart />
+            </div>
+          </div>
+          <div className={styles.barSection}>
+            <div className={styles.barTitle}>{'바 차트 이름'}</div>
+            <div className={styles.barContent}>
+              {/* 차트 들어갈 곳 */}
+              <BarChart />
+            </div>
+          </div>
+        </div>
+        <div className={styles.blankBox}></div>
+        <div className={styles.blankBox}></div>
+      </div>
+      {/* 표 영역 */}
+      <div className={styles.bottomContent}>
+        <div className={styles.bottomTitle}>
+          <span className={styles.resultCount}>검색된 패널 : {100}명</span>
+          <div className={styles.exportContainer}>
+            <ExportSelect />
+            <div className={styles.exportButton}>내보내기</div>
+          </div>
+        </div>
+        <PanelTable />
+      </div>
+    </div>
   );
 };
 
