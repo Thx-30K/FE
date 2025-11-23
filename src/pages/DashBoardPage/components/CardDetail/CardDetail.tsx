8842daa51e5a84bc9cc7d9206b3f2ebcf4443c6a
구혁모 <gurah129@gmail.com>
2025-11-23 15:55:54 +0900

    ♻️ 카드 더보기 테이블 재사용

--- src/pages/DashBoardPage/components/CardDetail/CardDetail.tsx
+++ src/pages/DashBoardPage/components/CardDetail/CardDetail.tsx
@@ -5,6 +5,7 @@ import styles from './CardDetail.module.scss';
 import { cardCloseIcon } from '@/assets';
 import { ExportSelect } from '../ExportSelect/ExportSelect';
 import { useEffect } from 'react';
+import { PanelTable } from '../Table/PanelTable';
 
 export const CardDetail = ({ data, onClick }: CardProps) => {
   useEffect(() => {
@@ -62,60 +63,7 @@ export const CardDetail = ({ data, onClick }: CardProps) => {
               <div className={styles.exportButton}>내보내기</div>
             </div>
           </div>
-          <table>
-            <colgroup>
-              <col style={{ width: '50px' }} />
-              <col style={{ width: '10px' }} />
-              <col style={{ width: '150px' }} />
-              <col style={{ width: '150px' }} />
-              <col style={{ width: '150px' }} />
-              <col style={{ width: '150px' }} />
-              <col style={{ width: '150px' }} />
-            </colgroup>
-            <thead>
-              <tr>
-                <th>No.</th>
-                <th>패널 ID</th>
-                <th>나이</th>
-                <th>월소득</th>
-                <th>지역</th>
-                <th>담배</th>
-                <th>직업</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* 추후 변경 */}
-              {[...Array(10)].map((_, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>w100010279508856</td>
-                  <td>25</td>
-                  <td>300만원</td>
-                  <td>서울</td>
-                  <td>비흡연</td>
-                  <td>학생</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className={styles.tableNav}>
-            <span className={styles.prevButton} onClick={() => {}}>
-              ◀
-            </span>
-            <div className={styles.tableNumbers}>
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((page) => (
-                <span
-                  key={page}
-                  className={`${styles.active} ${styles.tableNum}`}
-                >
-                  {page}
-                </span>
-              ))}
-            </div>
-            <span className={styles.nextButton} onClick={() => {}}>
-              ▶
-            </span>
-          </div>
+          <PanelTable />
         </div>
       </div>
     </div>
