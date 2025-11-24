3b49809b27fdb8c87b6dab6e9737eff45ff2be45
구혁모 <gurah129@gmail.com>
2025-11-25 00:41:16 +0900

    ✨ 리액트 쿼리 최상위 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import { createRoot } from 'react-dom/client';
 import '@/styles/global.scss';
 import App from './App.tsx';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1, // API 실패 시 1번만 재시도
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
